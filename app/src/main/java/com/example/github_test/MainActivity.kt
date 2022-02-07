5f58144f06a50d9b49da594bf8b2d3c362a444e8
iamraajkanchan <iamraajkanchan@gmail.com>
2022-02-07 17:38:27 +0530

    Changes made by Raj Kanchan on Activity_02022022

--- app/src/main/java/com/example/github_test/MainActivity.kt
+++ app/src/main/java/com/example/github_test/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.github_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.example.github_test.utils.Util
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(Util.TAG, "MainActivity :: onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(Util.TAG, "MainActivity :: onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(Util.TAG, "MainActivity :: onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(Util.TAG, "MainActivity :: onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(Util.TAG, "MainActivity :: onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(Util.TAG, "MainActivity :: onDestroy")
+    }
+
+    fun clickNext(view: View) {
+        if (view.id == R.id.btnClick) {
+            Intent(applicationContext, SecondActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(this)
+                finish()
+            }
+        }
     }
 }
